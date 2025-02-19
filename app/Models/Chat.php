d3805928a07c3da54fdbf4fd7e92825f82fd0724
devTheSagar <sagarthewebdev@gmail.com>
2025-02-19 13:55:34 +0600

    admin r chat model a relation add kora hoyeche

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chat extends Model
 {
-    //
+    use HasFactory;
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    // Relationship to get receiver profile
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function receiverProfilee()
+    {
+        return $this->belongsTo(Admin::class, 'receiver_id', 'id')->select(['id', 'name', 'picture', 'bio']);
+    }
+
+    public function senderProfilee()
+    {
+        return $this->belongsTo(Admin::class, 'sender_id', 'id')->select(['id', 'name', 'picture', 'bio']);
+    }
+     
+    public function receiverSellerProfile()
+    {
+        return $this->belongsTo(User::class, 'receiver_id', 'id')->select(['id', 'name', 'picture', 'bio']);
+    }
+
+    public function senderSellerProfile()
+    {
+        return $this->belongsTo(User::class, 'sender_id', 'id')->select(['id', 'name', 'picture', 'bio']);
+    }
 }
